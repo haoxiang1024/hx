0063fae4642d9eb9fc0751f7423db136ba9daf38
haoxiang <a3502777299@outlook.com>
2023-06-10 09:26:45 +0800

    commit:2023/06/10, 9:26:45

--- git_push.bat
+++ git_push.bat
@@ -5,7 +5,7 @@ cd /d %~dp0
 :: 自动提交
 git init 
 git add . 
- git commit -m "bat推送:%date:~0,10%,%time:~0,8%" 
+ git commit -m "commit:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
 @echo 已经完成
