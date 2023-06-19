d5ac0dca1bac73a8a23f7bc7ea4c54ba7a9f52b7
haoxiang <a3502777299@outlook.com>
2023-06-19 18:10:40 +0800

    commit:2023/06/19,18:10:40

--- school/src/main/java/com/school/config/RedisConfig.java
+++ school/src/main/java/com/school/config/RedisConfig.java
@@ -3,6 +3,8 @@ package com.school.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,10 +12,27 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(factory);
+//        template.setKeySerializer(new StringRedisSerializer()); // 设置键序列化器
+//        template.setValueSerializer(new GenericToStringSerializer<>(Object.class)); // 设置值序列化器
+//        return template;
+//    }
+
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("127.0.0.1", 6379);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer()); // 设置键序列化器
         template.setValueSerializer(new GenericToStringSerializer<>(Object.class)); // 设置值序列化器
         return template;
